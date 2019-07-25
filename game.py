4273d0b143759bafcad3dc0f687521c51cbf7ad3
s-walrus <swalrus@yandex.ru>
2019-07-26 00:14:38 +0300

    Added hand placing mechanics; edited game status system

--- game.py
+++ game.py
@@ -2,6 +2,11 @@ import random
 
 
 class Game:
+    # constants
+    __NOT_STARTED = 0
+    __FREE_CHAT = 1
+    __CONCENTRATION = 2
+    __ACTION = 3
     # initialized on init
     game_id = None
     # initialized on start
@@ -12,9 +17,7 @@ class Game:
     player_hands = dict()
     player_stops = dict()
     level = 0
-    level_started = False
-    game_started = False
-    is_stopped = False
+    status = 0
     rewards = [0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1]
     n_shurikens = 1
     top_card = 0
@@ -36,7 +39,9 @@ class Game:
 
     # finish the current level
     def finish_level(self):
-        self.level_started = False
+        if self.status == self.__ACTION or \
+                self.status == self.__CONCENTRATION:
+            self.status = self.__FREE_CHAT
 
     # returns the current game state as dict object
     # this object is returned by every public function
@@ -51,43 +56,42 @@ class Game:
     def add_player(self, player_id):
         if not self.game_started:
             self.player_hands[player_id] = set()
+            self.player_stops[player_id] = False
             self.n_players += 1
         else:
             raise RuntimeError('A player was added after the game had been started.')
         return self.get_status()
 
     def start_game(self):
-        self.game_started = True
-        if self.n_players == 2:
-            self.n_levels = 12
-            self.hp = 2
-        elif self.n_players == 3:
-            self.n_levels = 10
-            self.hp = 3
-        elif self.n_players == 4:
-            self.n_levels = 8
-            self.hp = 4
-        else:
-            raise ValueError("n_levels should be either 2, 3, or 4")
+        if self.status == self.__NOT_STARTED:
+            self.status = self.__FREE_CHAT
+            if self.n_players == 2:
+                self.n_levels = 12
+                self.hp = 2
+            elif self.n_players == 3:
+                self.n_levels = 10
+                self.hp = 3
+            elif self.n_players == 4:
+                self.n_levels = 8
+                self.hp = 4
+            else:
+                raise ValueError("n_levels should be either 2, 3, or 4")
         return self.get_status()
 
     # proceed to the next level
-    # use force=True to start the next level
-    # before finishing the currect one
-    def start_level(self, force=False):
-        if not self.level_started or force:
+    def start_level(self):
+        if self.status == self.__FREE_CHAT:
             self.level += 1
-            self.level_started = True
+            self.status = self.__CONCENTRATION
             self.pass_cards(self.level)
-        else:
-            print("It was attempted to start a level before " +
-                  "finishing the previous one. Use 'start_level(force=True)' " +
-                  "to force start a level.")
+            # everyone places their hand on the table
+            for player_id in self.player_stops.keys():
+                self.place_hand(player_id)
         return self.get_status()
 
     # place a card to the stack
     def act(self, player_id, card):
-        if card in self.player_hands[player_id]:
+        if self.status == self.__ACTION and card in self.player_hands[player_id]:
             self.player_hands[player_id].remove(card)
             self.top_card = card
             flag = False
@@ -107,6 +111,22 @@ class Game:
                   "Use 'act(player_id, card, force=True)' " +
                   "to force play a card.")
 
+        return self.get_status()
+
+    def place_hand(self, player_id):
+        if self.status == self.__ACTION:
+            self.player_stops[player_id] = True
+            self.status = self.__CONCENTRATION
+
+    def release_hand(self, player_id):
+        if self.status == self.__CONCENTRATION:
+            self.player_stops[player_id] = False
+            if sum(self.player_stops.values()) == 0:
+                self.status = self.__ACTION
+        return self.get_status()
+
+    def stop(self, player_id):
+        self.place_hand(player_id)
         return self.get_status()
 
 
