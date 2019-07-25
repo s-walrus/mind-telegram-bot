12639e5f156eb2398e1aa4243030028a4ea6d5df
iliamikado <iliamikado@gmail.com>
2019-07-25 13:32:01 +0300

    Made function for game start

---
+++ rand.py
@@ -0,0 +1,16 @@
+import random
+
+
+def get_cards(player_count, level):
+    cards = []
+    a = [i for i in range(1, 101)]
+    random.shuffle(a)
+    for i in range(player_count):
+        cards.append([])
+        for j in range(level):
+            cards[i].append(a[i * level + j])
+
+    return cards
+
+a = get_cards(2, 2)
+print(a)
