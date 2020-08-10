121e7062de537ade3780d76bb5b046e021bd4d87
Fedor <fchikhachev@gmail.com>
2020-08-10 18:20:55 +0300

    removed one_time keyboards

--- keyboards_vk.py
+++ keyboards_vk.py
@@ -2,14 +2,14 @@ from vk_api.keyboard import VkKeyboard
 
 
 def begin_keyboard():
-    keyboard = VkKeyboard(one_time=True)
+    keyboard = VkKeyboard()
     keyboard.add_button('Участвую', 'default')
     keyboard.add_button('Начать игру', 'positive')
     return keyboard.get_keyboard()
 
 
 def game_keyboard():
-    keyboard = VkKeyboard(one_time=True)
+    keyboard = VkKeyboard()
     keyboard.add_button('Ход', 'positive')
     keyboard.add_button('СТОП!', 'negative')
     keyboard.add_button('Сюрикен', 'primary')
@@ -18,20 +18,20 @@ def game_keyboard():
 
 
 def place_hand_keyboard():
-    keyboard = VkKeyboard(one_time=True)
+    keyboard = VkKeyboard()
     keyboard.add_button('СТОП!', 'negative')
     keyboard.add_button('Отпустить руку', 'positive')
     return keyboard.get_keyboard()
 
 
 def stop_keyboard():
-    keyboard = VkKeyboard(one_time=True)
+    keyboard = VkKeyboard()
     keyboard.add_button('Продолжить', 'positive')
     return keyboard.get_keyboard()
 
 
 def concentration_keyboard():
-    keyboard = VkKeyboard(one_time=True)
+    keyboard = VkKeyboard()
     keyboard.add_button('Отпустить руку', 'positive')
     return keyboard.get_keyboard()
 
@@ -41,6 +41,6 @@ def empty_keyboard():
 
 
 def start_level_keyboard():
-    keyboard = VkKeyboard(one_time=True)
+    keyboard = VkKeyboard()
     keyboard.add_button('Начать уровень', 'positive')
     return keyboard.get_keyboard()
